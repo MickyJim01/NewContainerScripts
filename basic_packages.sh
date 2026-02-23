2ce920a12d2f09ada542f01891308dc29f1ac9a6
Michael McGuire <158031884+MickyJim01@users.noreply.github.com>
2026-02-23 17:55:59 +1100

    Install Docker and Komodo periphery container
    Added installation steps for Docker and Komodo periphery container.

--- basic_packages.sh
+++ basic_packages.sh
@@ -13,4 +13,14 @@ echo "Installing custom packages:
   - tmux"
 apt install -y cifs-utils nfs-common tree curl tmux
 
+echo "installing docker..."
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+rm get-docker.sh
+echo "docker installed!"
+
+echo "installing komodo periphery container..."
+curl -sSL https://raw.githubusercontent.com/moghtech/komodo/main/scripts/setup-periphery.py | python3
+systemctl enable periphery
+echo "please check status of periphery"
+
 echo "Done!"
